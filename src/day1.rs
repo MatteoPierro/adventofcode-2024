30adceeae813941a1fea0ddf6ad0087db6e13277
Matteo Pierro <pierromatteo@gmail.com>
2024-12-01 11:09:21 +0200

    Parses the line to integers

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,39 @@
 #[cfg(test)]
 mod tests {
+    use indoc::indoc;
+    use itertools::Itertools;
     use crate::input_reader::*;
 
     #[test]
-    fn it_works() {
-        let input = read_input_file("test.txt");
-        let lines = read_lines(&input);
-        assert_eq!(vec!["some text", "here and", "there"], lines);
+    fn it_parses_the_input() {
+        let input = indoc! {"
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        "};
+        let (c1, c2) = parse_input(&input);
+        assert_eq!(vec![3, 4, 2, 1, 3, 3], c1);
+        assert_eq!(vec![4, 3, 5, 3, 9, 3], c2);
+    }
+
+
+    #[test]
+    fn it_parses_a_line() {
+        let line = "3   4";
+        assert_eq!((3, 4), parse_line(line))
+    }
+
+    fn parse_line(line: &str) -> (usize, usize) {
+        line.split("   ")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect_tuple()
+            .unwrap()
+    }
+
+    fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
+        todo!()
     }
 }
