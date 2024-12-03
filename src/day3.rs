fb120e0c2187f811b1cbddf540f6200e4231c34b
Matteo Pierro <pierromatteo@gmail.com>
2024-12-03 13:39:43 +0200

    it solves first puzzle day 03

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,27 @@
 #[cfg(test)]
 mod tests {
+    use regex::Regex;
+    use crate::input_reader::{read_input_file, read_lines};
+
     #[test]
-    fn it_works() {
-        assert_eq!(false, true)
+    fn it_calculates_the_sum_of_multiplications() {
+        let line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(161, mul_sum_line(line))
+    }
+
+    #[test]
+    fn it_solve_first_puzzle() {
+        let total: usize = read_lines(&read_input_file("input_03"))
+            .iter().map(|l| mul_sum_line(l))
+            .sum();
+        assert_eq!(187194524, total)
+    }
+
+    fn mul_sum_line(line: &str) -> usize {
+        Regex::new(r"mul\((\d+),(\d+)\)").unwrap()
+            .captures_iter(line)
+            .map(|c| c.extract())
+            .map(|(_, [first, second])| first.parse::<usize>().unwrap() * second.parse::<usize>().unwrap())
+            .sum()
     }
 }
