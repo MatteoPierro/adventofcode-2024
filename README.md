d92d7df20310633e1f6343028783f26ead5d095b
Matteo Pierro <pierromatteo@gmail.com>
2024-12-04 11:05:06 +0200

    it solves second puzzle day 04

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ cargo test dayXY::tests
 - [Day 1: Historian Hysteria](./src/day1.rs)
 - [Day 2: Red-Nosed Reports](./src/day2.rs)
 - [Day 3: Mull It Over](./src/day3.rs)
+- [Day 4:Ceres Search](./src/day4.rs)
--- src/day4.rs
+++ src/day4.rs
@@ -28,6 +28,67 @@ mod tests {
         assert_eq!(2718, count_xmas(input))
     }
 
+    #[test]
+    fn it_count_x_mas() {
+        let input = indoc! {"
+        .M.S......
+        ..A..MSMS.
+        .M.S.MAA..
+        ..A.ASMSM.
+        .M.S.M....
+        ..........
+        S.S.S.S.S.
+        .A.A.A.A..
+        M.M.M.M.M.
+        .........."};
+
+        assert_eq!(9, count_x_mas(input))
+    }
+
+    #[test]
+    fn it_solves_second_puzzle() {
+        let input = &read_input_file("input_04");
+
+        assert_eq!(2046, count_x_mas(input))
+    }
+
+    fn count_x_mas(input: &str) -> usize {
+        let parsed_input: Vec<_> = read_lines(input).iter()
+            .map(|l| l.chars().collect::<Vec<_>>())
+            .collect();
+
+        parsed_input
+            .clone()
+            .into_iter()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.iter().enumerate().filter_map(|(x, &char)| {
+                    if char == 'A' {
+                        Some((x as isize, y as isize))
+                    } else {
+                        None
+                    }
+                }).collect::<Vec<_>>()
+            })
+            .filter(|&s| filter_x_mas(&parsed_input, s))
+            .count()
+    }
+
+    fn filter_x_mas(parsed_input: &Vec<Vec<char>>, start: (isize, isize)) -> bool {
+        let words = [
+            vec![(-1, -1), (0, 0), (1, 1)],
+            vec![(-1, 1), (0, 0), (1, -1)],
+        ].iter()
+            .map(|d| navigate_to_direction(&parsed_input, start, d))
+            .collect::<Vec<_>>();
+
+        if words.len() != 2 {
+            return false
+        }
+
+        words.iter().all(|w| w == "MAS" || w == "SAM")
+    }
+
     fn count_xmas(input: &str) -> usize {
         let parsed_input: Vec<_> = read_lines(input).iter()
             .map(|l| l.chars().collect::<Vec<_>>())
@@ -46,27 +107,27 @@ mod tests {
                     }
                 }).collect::<Vec<_>>()
             })
-            .flat_map(|s| navigate(&parsed_input, s))
+            .flat_map(|s| navigate_xmas(&parsed_input, s))
             .filter(|w| w == "XMAS")
             .count()
     }
 
-    fn navigate(parsed_input: &Vec<Vec<char>>, start: (isize, isize)) -> Vec<String> {
+    fn navigate_xmas(parsed_input: &Vec<Vec<char>>, start: (isize, isize)) -> Vec<String> {
         [
-            [(0, 0), (0, 1), (0, 2), (0, 3)],
-            [(0, 0), (0, -1), (0, -2), (0, -3)],
-            [(0, 0), (1, 0), (2, 0), (3, 0)],
-            [(0, 0), (-1, 0), (-2, 0), (-3, 0)],
-            [(0, 0), (1, 1), (2, 2), (3, 3)],
-            [(0, 0), (-1, -1), (-2, -2), (-3, -3)],
-            [(0, 0), (1, -1), (2, -2), (3, -3)],
-            [(0, 0), (-1, 1), (-2, 2), (-3, 3)],
+            vec![(0, 0), (0, 1), (0, 2), (0, 3)],
+            vec![(0, 0), (0, -1), (0, -2), (0, -3)],
+            vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+            vec![(0, 0), (-1, 0), (-2, 0), (-3, 0)],
+            vec![(0, 0), (1, 1), (2, 2), (3, 3)],
+            vec![(0, 0), (-1, -1), (-2, -2), (-3, -3)],
+            vec![(0, 0), (1, -1), (2, -2), (3, -3)],
+            vec![(0, 0), (-1, 1), (-2, 2), (-3, 3)],
         ].iter()
-            .map(|d| navigate_to_direction(&parsed_input, start, *d))
+            .map(|d| navigate_to_direction(&parsed_input, start, d))
             .collect()
     }
 
-    fn navigate_to_direction(map: &Vec<Vec<char>>, (x, y): (isize, isize), direction: [(isize, isize); 4]) -> String {
+    fn navigate_to_direction(map: &Vec<Vec<char>>, (x, y): (isize, isize), direction: &Vec<(isize, isize)>) -> String {
         direction.iter().filter_map(|(dx, dy)| {
             map.get((y + *dy) as usize)?.get((x + *dx) as usize)
         }).collect()
